3168e4762cd4afc32e030fb99bf01f4880774488
MrNish <patelnish0512@gmail.com>
2025-01-08 16:42:33 +0530

    Initial Commit

---
+++ Basic.py
@@ -0,0 +1,15 @@
+# Scripting language
+print(" Hello World!")
+
+# procedural language
+def demo():
+    print("Pyhton is procedural Language")
+    print("This white space is for identation: it refers block of scope")
+demo()
+
+# Object Oriented Language
+class Demo:
+    def display(self):
+        print("Python is object oriented language")
+d1 = Demo()
+d1.display()
---
+++ VariablesDataTypes/Overview.py
@@ -0,0 +1,3 @@
+#1. Python is Dynamically typed programming language
+#2. In Pyhton while declaring variables, we do need to mention Data types.
+#3. Data types will be assigned by the langauage based on value stored into the variables.
